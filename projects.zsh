f1095c7b46ca966004a47fe0951603833d616a37
”FartDraft” <fartdraft@gmail.com>
2023-12-01 04:16:09 +0300

    well, better

--- projects.zsh
+++ projects.zsh
@@ -7,63 +7,102 @@ names=() descriptions=() paths=()
 names_max_len=0 descriptions_max_len=0 paths_max_len=0
 total=0 lines=0
 
-function add_line() {
-    case $((lines % 3)) in
-        0)
-            names+=("$1")
-            if ((${#1} > names_max_len)); then
-                names_max_len=${#1}
-            fi
-        ;;
-        1)
-            descriptions+=("$1")
-            if ((${#1} > descriptions_max_len)); then
-                descriptions_max_len=${#1}
-            fi
-        ;;
-        2)
-            paths+=("$1")
-            if ((${#1} > paths_max_len)); then
-                paths_max_len=${#1}
-            fi
-            ((total++))
-        ;;
-    esac
-    ((lines++))
-}
+function read_projects() {
+    names=() descriptions=() paths=()
+    names_max_len=0 descriptions_max_len=0 paths_max_len=0
+    total=0 lines=0
 
-# Read from $file
-while read -r line; do
-    add_line "$line"
-done < "$file"
+    while read -r line; do
+        case $((lines % 3)) in
+            0)
+                names+=("$line")
+                if ((${#line} > names_max_len)); then
+                    names_max_len=${#line}
+                fi
+            ;;
+            1)
+                descriptions+=("$line")
+                if ((${#line} > descriptions_max_len)); then
+                    descriptions_max_len=${#line}
+                fi
+            ;;
+            2)
+                paths+=("$line")
+                if ((${#line} > paths_max_len)); then
+                    paths_max_len=${#line}
+                fi
+                ((total++))
+            ;;
+        esac
+        ((lines++))
+    done < "$file"
+}
 
 reset="\033[0m"
 yellow="\033[1;33m"
-red="\033[0;31m"
+green="\033[0;32m"
 blue="\033[0;34m"
 function list() {
-    echo "Projects:"
-    for lines in {1..$total}; do
-        echo -n    "    $lines. "
-        echo -n -e "${yellow}${(r($names_max_len)(  ))names[$lines]} "
-        echo -n -e "${red}${(r($descriptions_max_len)(  ))descriptions[$lines]} "
-        echo    -e "${blue}${(r($paths_max_len)(  ))paths[$lines]}${reset}"
-    done
+    read_projects
+    if [[ $total == 0 ]]; then
+        echo "No projects yet"
+    else
+        echo "Projects:"
+        for line in {1..$total}; do
+            echo -n    "    $line. "
+            echo -n -e "${yellow}${(r($names_max_len)(  ))names[$line]} "
+            echo -n -e "${green}${(r($descriptions_max_len)(  ))descriptions[$line]} "
+            echo    -e "${blue}${(r($paths_max_len)(  ))paths[$line]}${reset}"
+        done
+    fi
 }
 
 function add() {
+    read_projects
     if [[ -z $1 ]]; then
         echo "Usage: projects add <name> <description>"
     else
-        echo "$1" >> "$file"
-        add_line "$1"
-        echo "$2" >> "$file"
-        add_line "$2"
-        echo "$PWD" >> $file
-        add_line "$PWD"
+        for i in {1..$total}; do
+            if [[ $1 == ${names[i]} ]]; then
+                echo "Project with the same name already exists"
+                return
+            elif [[ $PWD == ${paths[i]} ]]; then
+                echo "Project with the same path already exists"
+                return
+            fi
+        done
+
+        echo "$1"   >> "$file"
+        echo "$2"   >> "$file"
+        echo "$PWD" >> "$file"
     fi
+}
+
+function delete() {
+    read_projects
+    line=-1
+    if [[ -z $1 ]]; then
+        echo "Usage: projects delete <name>"
+    else
+        for i in {1..$total}; do
+            if [[ $1 == ${names[i]} ]]; then
+                line=$((1 + 3*(i-1)))
+                sed -i "${line}d" "$file"
+                sed -i "${line}d" "$file"
+                sed -i "${line}d" "$file"
+                break
+            fi
+        done
+        if [[ $line == -1 ]]; then
+            echo "No such project"
+        else
+            ((lines-=3))
+            ((total--))
+            read_projects
+        fi
     fi
 }
 
 alias p=list
 alias pa=add
+alias pd=delete
